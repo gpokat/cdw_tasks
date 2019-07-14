fd55461251aed3ca15151662fbeac13a325ff6ee
gleb <glebyshek92@mail.ru>
2019-07-14 11:41:14 +0300

    Create 6kyu_Sum_of_Digits_Digital_Root.cpp

---
+++ 6kyu_Sum_of_Digits_Digital_Root.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+
+int digital_root(int n)
+{
+
+//if(n<=9) return n;
+
+    string line=std::to_string(n);
+     int digit=0; 
+     
+    for(int i=0;i<line.length(); i++){
+      
+      digit+=atoi(line.substr(i,1).c_str());
+     
+    
+    }
+
+if(digit>=10) return digital_root(digit);
+else {cout<<"digi: "<<digit<<endl; return  digit; }    
+}
